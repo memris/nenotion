e37af4d083f803400201a747136c52d4cb5cd026
memris <michurinaalyona@gmail.com>
2024-10-09 23:40:17 +0300

    Initial Commit

---
+++ nenotion/nenotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  nenotionApp.swift
+//  nenotion
+//
+//  Created by USER on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct nenotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
